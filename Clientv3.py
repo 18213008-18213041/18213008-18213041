d4973676a6a9f0c7b659c0bf241d5c7fc3fef4f4
nicolosinapitupulu <nicolosinapitupulu@gmail.com>
2015-09-17 12:40:44 +0700

    Create Clientv3.py

---
+++ Clientv3.py
@@ -0,0 +1,47 @@
+# Name/NIM	: Nicholas Posma Nasution/18213008 - Indra Nicolosi Waskita/18213041
+# Day, Date	: Wednesday, 16 September 2015
+# File		: Clientv3.py
+
+import socket, sys
+
+if __name__ == "__main__":
+	if(len(sys.argv) < 3):
+		print "Usage : phyton client.py hostname port"
+		sys.exit()
+	host = sys.argv[1]
+	port = int(sys.argv[2])
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.settimeout(5)
+
+	try:
+		sock.connect((host,port))
+	except:
+		print "Unable to connect"
+		sys.exit()
+	print "Connected to server. Type '@bye' to end the connection"
+
+	rsp = sock.recv(1024)
+	print "Menu: " + rsp + "\n"
+
+	msg = ""
+
+	while(msg != "@bye"):
+		sys.stdout.write("Input : ")
+		msg = sys.stdin.readline()
+		msg = msg.rstrip('\n')
+		sock.send(msg)
+
+		rsp = sock.recv(1024)
+		if(rsp.endswith(".txt")):
+			with open(rsp, "wb") as file_to_write:
+				data = sock.recv(1024)
+				if not data:
+				    break
+				file_to_write.write(data)
+			print "Server wrote: file sent!\n"
+		else:
+			if(rsp != "disconnected"):
+				print "Server wrote: " + rsp +"\n"
+	print "You are disconnected"
+	sock.close()

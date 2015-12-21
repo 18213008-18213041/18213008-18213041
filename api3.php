18d64affd2fab6a228d576d9a4e1a703c185a091
nicolosinapitupulu <nicolosinapitupulu@gmail.com>
2015-12-21 15:19:46 +0700

    Create api3.php

---
+++ api3.php
@@ -0,0 +1,66 @@
+<?php
+	/* Name/NIM		: Nicholas Posma Nasution Indra Nicolosi Waskita/18213041 */
+	/* Date			: Tuesday, 24 November 2015 */
+
+	function searchEvent($criteria) {
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "dbprogif";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		$rows = array();
+
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+			$rows = "fail";
+		} else {
+			$sql = "SELECT * FROM datakader WHERE Nama LIKE '%" . $criteria .  "%' OR NIM LIKE '%" . $criteria ."%'";
+			$result = $conn->query($sql);
+			while($r = $result->fetch_assoc()){
+				$rows[] = array('data' => $r);
+			}
+		}
+
+		$conn->close();
+		return $rows;
+	}
+
+	function sortEvent($criteria, $order) {
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "dbprogif";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		$rows = array();
+
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+			$rows = "fail";
+		} else {
+			$sql = "SELECT * FROM datakader ORDER BY $criteria" . " " . $order;
+			$result = $conn->query($sql);
+			while($r = $result->fetch_assoc()){
+				$rows[] = array('data' => $r);
+			}
+		}
+
+		$conn->close();
+		return $rows;
+	}
+
+	if(isset($_GET["action"])) {
+		switch($_GET["action"]) {
+			case "search";
+				$rows = searchEvent($_GET["criteria"]);
+				break;
+			case "sort";
+				$rows = sortEvent($_GET["criteria"], $_GET["order"]);
+				break;
+		}
+		exit(json_encode($rows));
+	} else {
+
+	}
+?>

acf46a98a6e3a8c450f9c13ee360029aa0bd518e
nicolosinapitupulu <nicolosinapitupulu@gmail.com>
2016-03-25 14:10:24 +0700

    Create esp1.c

---
+++ esp1.c
@@ -0,0 +1,126 @@
+// http://www.arduinesp.com
+
+//
+
+// Plot DTH11 data on thingspeak.com using an ESP8266
+
+// April 11 2015
+
+// Author: Jeroen Beemster
+
+// Website: http://www.arduinesp.com
+
+#include <ESP8266WiFi.h>
+
+// replace with your channel’s thingspeak API key,
+
+String apiKey = “****”;
+
+const char* ssid = “andrawifi”;
+
+const char* password = “********”;
+
+const char* server = “api.thingspeak.com”;
+
+//#define DHTPIN 2 // what pin we’re connected to
+
+//DHT dht(DHTPIN, DHT11,15);
+
+WiFiClient client;
+
+float temp;
+
+int tempPin = 0;
+
+void setup() {
+
+Serial.begin(115200);
+
+delay(10);
+
+//  dht.begin();
+
+WiFi.begin(ssid, password);
+
+Serial.println();
+
+Serial.println();
+
+Serial.print(“Connecting to “);
+
+Serial.println(ssid);
+
+WiFi.begin(ssid, password);
+
+while (WiFi.status() != WL_CONNECTED) {
+
+delay(500);
+
+Serial.print(“.”);
+
+}
+
+Serial.println(“”);
+
+Serial.println(“WiFi connected”);
+
+}
+
+void loop() {
+
+temp = analogRead(tempPin);
+
+temp = temp * 0.48828125;
+
+if (isnan(temp)) {
+
+Serial.println(“Failed to read from LM35 sensor!”);
+
+return;
+
+}
+
+if (client.connect(server,80)) {  //   “184.106.153.149” or api.thingspeak.com
+String postStr = apiKey;
+
+postStr +=”&field1=”;
+
+postStr += String(temp);
+
+postStr += “\r\n\r\n”;
+
+client.print(“POST /update HTTP/1.1\n”);
+
+client.print(“Host: api.thingspeak.com\n”);
+
+client.print(“Connection: close\n”);
+
+client.print(“X-THINGSPEAKAPIKEY: “+apiKey+”\n”);
+
+client.print(“Content-Type: application/x-www-form-urlencoded\n”);
+
+client.print(“Content-Length: “);
+
+client.print(postStr.length());
+
+client.print(“\n\n”);
+
+client.print(postStr);
+
+Serial.print(“Temperature: “);
+
+Serial.print(temp);
+
+Serial.println(“% send to Thingspeak”);
+
+}
+
+client.stop();
+
+Serial.println(“Waiting…”);
+
+// thingspeak needs minimum 15 sec delay between updates
+
+delay(20000);
+
+}

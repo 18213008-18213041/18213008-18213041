3faf79491d2db9cf1b081dde871ce49f5db8559e
nicolosinapitupulu <nicolosinapitupulu@gmail.com>
2016-04-14 09:32:58 +0700

    Update temperature.ino

--- temperature.ino
+++ temperature.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>  
-#define SSID ""    // SSID
-#define PASS ""                // Password
-#define HOST ""  // Webhost
+#define SSID "andrawifi"    // SSID
+#define PASS "20160128"                // Password
+#define HOST "192.168.88.13"  // Webhost
 #define DEBUG true
 
 float temp;
@@ -11,32 +11,30 @@ SoftwareSerial Serial1(2,3);
 void setup() {
   // put your setup code here, to run once:
   delay(5000);
-  //connectWifi();
+  connectWifi();
   Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sendCommand("AT+GMR",3000,DEBUG);
-/*  temp = analogRead(tempPin);
+  temp = analogRead(tempPin);
   temp = temp * 0.48828125;
   sendData(temp);
   delay(3000);
   Serial.print("Temperature: ");
   Serial.print(temp);
   Serial.print(" *C");
-  Serial.println(); */
+  Serial.println();
 }
 
 void connectWifi() {
   //Set-wifi
-/*  sendCommand("AT+RST\r\n",2000,DEBUG); // reset module
+  sendCommand("AT+RST\r\n",2000,DEBUG); // reset module
   sendCommand("AT+CWMODE=1\r\n",1000,DEBUG); // configure as access point
-  sendCommand("AT+CWJAP=\" \",\" \"\r\n",3000,DEBUG);
+  sendCommand("AT+CWJAP=\"andrawifi\",\"20160128\"\r\n",3000,DEBUG);
   delay(5000);
   sendCommand("AT+CIFSR\r\n",1000,DEBUG); // get ip address
-  delay(2000); */
-  sendCommand("AT+CIPSTART=\"TCP\",\"www.google.com\",80\r\n",3000,DEBUG);
+  delay(2000);
 }
 
 void sendData(float temp) {
@@ -49,7 +47,7 @@ void sendData(float temp) {
   String cmd2 = "GET /imka/server.php?temperature="; // Link ke skrip PHP                    
   cmd2 += temp;          // Nilai Temperatur
   cmd2 += " HTTP/1.0\r\n";
-  // cmd2 += "Host: 192.168.88.13\r\n\r\n\r\n"; // Host
+  cmd2 += "Host: 192.168.88.13\r\n\r\n\r\n"; // Host
   String pjg="AT+CIPSEND=";
   pjg += cmd2.length();
   pjg += "\r\n";
